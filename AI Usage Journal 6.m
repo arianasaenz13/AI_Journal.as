e94110b7d256f6dce376794cd1444d44ea44f039
arianasaenz13 <ariana.saenz13@icloud.com>
2025-10-23 00:16:23 -0400

    Create AI Usage Journal 6.m

---
+++ AI Usage Journal 6.m
@@ -0,0 +1,10 @@
+AI Usage Journal
+Ariana Saenz
+asaenz13@terpmail.umd.edu
+Section 0303
+
+Week 6
+
+This week I used AI to help me understand better why it is essential to add @staticmethod when writing code with such. This is a link of the answer ChatGPT gave me when asking why it is so important: https://chatgpt.com/c/68f9aaeb-5170-8328-a5dc-f9eb3eaa6a7d. 
+
+Using AI allowed me to understand further in depth why it is essential. It clearly communicates the intent and how it behaves in relation to the class. 
